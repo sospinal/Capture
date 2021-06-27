6e9b92365ec49f0d2d1ed1b3dec7c6eedbf9dea6
Santiago Ospina Leal <sospinaleal@gmail.com>
2021-06-27 18:08:37 -0500

    13. Changes reverted
    Changes introduced for learning how to use the framer motion library are deleted.

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,41 +4,20 @@ import { About, Description, Image, Hide } from "../styles";
 import { motion } from "framer-motion";
 
 const AboutSection = () => {
-  const tittleAnimation = {
-    hidden: { opacity: 0 },
-    show: { opacity: 1, transition: { duration: 2 } },
-  };
-  const containerAnimation = {
-    hidden: { x: 100 },
-    show: {
-      x: 0,
-      transition: {
-        duration: 0.75,
-        ease: "easeOut",
-        staggerChildren: 1,
-        when: "afterChildren",
-      },
-    },
-  };
   return (
     <About>
       <Description>
-        <motion.div
-          variants={containerAnimation}
-          initial="hidden"
-          animate="show"
-          className="title"
-        >
+        <motion.div>
           <Hide>
-            <motion.h2 variants={tittleAnimation}>We work to make</motion.h2>
+            <motion.h2>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 variants={tittleAnimation}>
+            <motion.h2>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 variants={tittleAnimation}>true.</motion.h2>
+            <motion.h2>true.</motion.h2>
           </Hide>
           <p>
             Contact us for any photography or videography ideas that you have.
