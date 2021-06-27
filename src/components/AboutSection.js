e63b719e33b3a4f9ee167a6beaba30c6cdfb56bb
Santiago Ospina Leal <sospinaleal@gmail.com>
2021-06-27 18:04:41 -0500

    12. Framer motion testing
    A simple transitions are used in the AboutSection, specifically for the main tittle after navigation bar. This changes will eliminated as they were just for learning how to use the framer motion library. Final transitions will be added soon in a future commit.

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,30 +1,51 @@
 import React from "react";
 import home1 from "../img/home1.png";
-import styled from "styled-components";
 import { About, Description, Image, Hide } from "../styles";
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
+  const tittleAnimation = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 2 } },
+  };
+  const containerAnimation = {
+    hidden: { x: 100 },
+    show: {
+      x: 0,
+      transition: {
+        duration: 0.75,
+        ease: "easeOut",
+        staggerChildren: 1,
+        when: "afterChildren",
+      },
+    },
+  };
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div
+          variants={containerAnimation}
+          initial="hidden"
+          animate="show"
+          className="title"
+        >
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2 variants={tittleAnimation}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2 variants={tittleAnimation}>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true.</h2>
+            <motion.h2 variants={tittleAnimation}>true.</motion.h2>
           </Hide>
           <p>
             Contact us for any photography or videography ideas that you have.
             We have professionals with mazing skills.
           </p>
           <button>Contact us</button>
-        </div>
+        </motion.div>
         <Image>
           <img src={home1} alt="Guy with a camara" />
         </Image>
