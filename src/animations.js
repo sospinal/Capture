30810052cefbd3fd8cc7bab602b0cfc346bce79c
Santiago Ospina Leal <sospinaleal@gmail.com>
2021-07-18 19:52:02 -0500

    Scroll animations
    -A custom hook called useScroll is cretated. This makes that when
    scrolling if some part of the page gets inside the view it will trigger
    an animation.
    -The About styled component becomes motion.div, so it will allow to add
    Scroll animations to the different sections of the page that use it.
    -Scroll animations are implemented in ServiceSection and FaqSection.
    -ScrollReveal (animation) is added for fadding in and out if view shows
    certain percentage of a component (for now the threshold is 0.5).

--- src/animations.js
+++ src/animations.js
@@ -76,3 +76,14 @@ export const sliderContainer = {
   hidden: { opacity: 1 },
   show: { opacity: 1, transition: { ease: "easeOut", staggerChildren: 0.15 } },
 };
+
+export const scrollReveal = {
+  hidden: { opacity: 0, scale: 1.2, transition: { duration: 0.5 } },
+  show: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -3,10 +3,19 @@ import styled from "styled-components";
 import { About } from "../styles";
 import Toggle from "../components/Toggle";
 import { AnimateSharedLayout } from "framer-motion";
+import { useScroll } from "./useScroll";
+import { scrollReveal } from "../animations";
 
 const FaqSection = () => {
+  const [element, controls] = useScroll();
+
   return (
-    <Faq>
+    <Faq
+      variants={scrollReveal}
+      animate={controls}
+      initial="hidden"
+      ref={element}
+    >
       <h2>
         Any questions? <span>FAQ</span>
       </h2>
