476a927a703c1f1f06c2af4a032f55f32d646a9f
Santiago Ospina Leal <sospinaleal@gmail.com>
2021-07-23 12:38:02 -0500

    useScroll threshold cahnges with the view width
    A conditional is added in order to change the trigger threshold of the
    scroll animations to a lower number when the width is narrower.

--- src/components/useScroll.js
+++ src/components/useScroll.js
@@ -2,8 +2,13 @@ import { useInView } from "react-intersection-observer";
 import { useAnimation } from "framer-motion";
 
 export const useScroll = () => {
+  let tValue = 0.3;
+  if (window.innerWidth <= 450) {
+    tValue = 0.15;
+  }
+
   const controls = useAnimation();
-  const [element, view] = useInView({ threshold: 0.3 });
+  const [element, view] = useInView({ threshold: tValue });
 
   if (view) {
     controls.start("show");
