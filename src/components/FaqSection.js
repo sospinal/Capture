b3032bfd1871a7c0ce300ae708c54c55524242db
Santiago Ospina Leal <sospinaleal@gmail.com>
2021-07-26 09:48:14 -0500

    Overflow-x in mobile devices fixed
    There was some overflow on the right in mobile devices beacuse of the
    scaling of the animations, so the Hide component is used for wrapping
    Services and and Faq components.

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { About } from "../styles";
+import { About, Hide } from "../styles";
 import Toggle from "../components/Toggle";
 import { AnimateSharedLayout } from "framer-motion";
 import { useScroll } from "./useScroll";
@@ -10,54 +10,56 @@ const FaqSection = () => {
   const [element, controls] = useScroll();
 
   return (
-    <Faq
-      variants={scrollReveal}
-      animate={controls}
-      initial="hidden"
-      ref={element}
-    >
-      <h2>
-        Any questions? <span>FAQ</span>
-      </h2>
-      <AnimateSharedLayout>
-        <Toggle title="How do I start?">
-          <div className="answer">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
-              possimus.
-            </p>
-          </div>
-        </Toggle>
-        <Toggle title="Daily schedule">
-          <div className="answer">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
-              possimus.
-            </p>
-          </div>
-        </Toggle>
-        <Toggle title="Different payment methods">
-          <div className="answer">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
-              possimus.
-            </p>
-          </div>
-        </Toggle>
-        <Toggle title="What prodcuts do you offer">
-          <div className="answer">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
-              possimus.
-            </p>
-          </div>
-        </Toggle>
-      </AnimateSharedLayout>
-    </Faq>
+    <Hide>
+      <Faq
+        variants={scrollReveal}
+        animate={controls}
+        initial="hidden"
+        ref={element}
+      >
+        <h2>
+          Any questions? <span>FAQ</span>
+        </h2>
+        <AnimateSharedLayout>
+          <Toggle title="How do I start?">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+                possimus.
+              </p>
+            </div>
+          </Toggle>
+          <Toggle title="Daily schedule">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+                possimus.
+              </p>
+            </div>
+          </Toggle>
+          <Toggle title="Different payment methods">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+                possimus.
+              </p>
+            </div>
+          </Toggle>
+          <Toggle title="What prodcuts do you offer">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+                possimus.
+              </p>
+            </div>
+          </Toggle>
+        </AnimateSharedLayout>
+      </Faq>
+    </Hide>
   );
 };
 
--- src/components/ServiceSection.js
+++ src/components/ServiceSection.js
@@ -5,7 +5,7 @@ import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
 //styles
-import { About, Description, Image } from "../styles";
+import { About, Description, Image, Hide } from "../styles";
 import styled from "styled-components/macro";
 import { scrollReveal } from "../animations";
 
@@ -15,51 +15,53 @@ const ServicesSection = () => {
   const [element, controls] = useScroll();
 
   return (
-    <Services
-      variants={scrollReveal}
-      animate={controls}
-      initial="hidden"
-      ref={element}
-    >
-      <Description>
-        <h2>
-          High <span>quality</span> services
-        </h2>
-        <Cards>
-          <Card>
-            <div className="icon">
-              <img src={clock} alt="icon" />
-              <h3>Efficient</h3>
-            </div>
-            <p>Lorem ipsum dolor, sit amet.</p>
-          </Card>
-          <Card>
-            <div className="icon">
-              <img src={teamwork} alt="icon" />
-              <h3>Teamwork</h3>
-            </div>
-            <p>Lorem ipsum dolor, sit amet.</p>
-          </Card>
-          <Card>
-            <div className="icon">
-              <img src={diaphragm} alt="icon" />
-              <h3>Diaphragm</h3>
-            </div>
-            <p>Lorem ipsum dolor, sit amet.</p>
-          </Card>
-          <Card>
-            <div className="icon">
-              <img src={money} alt="icon" />
-              <h3>Affordable</h3>
-            </div>
-            <p>Lorem ipsum dolor, sit amet.</p>
-          </Card>
-        </Cards>
-      </Description>
-      <Image>
-        <img src={home2} alt="Camera" />
-      </Image>
-    </Services>
+    <Hide>
+      <Services
+        variants={scrollReveal}
+        animate={controls}
+        initial="hidden"
+        ref={element}
+      >
+        <Description>
+          <h2>
+            High <span>quality</span> services
+          </h2>
+          <Cards>
+            <Card>
+              <div className="icon">
+                <img src={clock} alt="icon" />
+                <h3>Efficient</h3>
+              </div>
+              <p>Lorem ipsum dolor, sit amet.</p>
+            </Card>
+            <Card>
+              <div className="icon">
+                <img src={teamwork} alt="icon" />
+                <h3>Teamwork</h3>
+              </div>
+              <p>Lorem ipsum dolor, sit amet.</p>
+            </Card>
+            <Card>
+              <div className="icon">
+                <img src={diaphragm} alt="icon" />
+                <h3>Diaphragm</h3>
+              </div>
+              <p>Lorem ipsum dolor, sit amet.</p>
+            </Card>
+            <Card>
+              <div className="icon">
+                <img src={money} alt="icon" />
+                <h3>Affordable</h3>
+              </div>
+              <p>Lorem ipsum dolor, sit amet.</p>
+            </Card>
+          </Cards>
+        </Description>
+        <Image>
+          <img src={home2} alt="Camera" />
+        </Image>
+      </Services>
+    </Hide>
   );
 };
 
